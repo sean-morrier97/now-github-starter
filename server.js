376fd1aa362f67ff16256a701f4d303292a172e5
sean-morrier97 <sean.morrier@hotmail.com>
2018-11-16 16:17:47 -0500

    docker added

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on {PORT}`);
